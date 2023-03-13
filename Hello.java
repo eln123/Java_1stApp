c3b841c670967faa1918a53581410d76bcb69b4e
Ethan <elnair122@gmail.com>
2023-03-13 14:02:40 -0400

    addedScanner

--- Hello.java
+++ Hello.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 /**
  * Hello
@@ -6,6 +7,13 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("please enter your name: ");
+        String name = scan.nextLine();
+        System.out.println("Hello " + name);
+        scan.close();
     }
 
 }
